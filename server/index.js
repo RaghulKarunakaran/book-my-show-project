d3c6a2410db0c10a6bbb0cc309e4202c32a4c557
RaghulKarunakarn <raghul.k@doodleblue.com>
2025-06-08 22:39:06 +0530

    home page backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const userRoutes = require('./routes/userRoutes');
+const movieRoutes = require('./routes/movieRoutes');
 const dburl = "mongodb+srv://kraghul1905:YdjjErJzUgSICrJr@cluster0.drdvh.mongodb.net/scaler_practice?retryWrites=true&w=majority&appName=Cluster0";
 
 require('dotenv').config();
@@ -13,6 +14,7 @@ mongoose.connect(dburl).then((function() {
 })).catch(err => console.log(err));
 
 app.use('/api/users', userRoutes);
+app.use('/api/movies', movieRoutes);
 
 app.listen(8081, () => {
     console.log("Server is Connected");
