ca08f593459ea1d5483cc6cbb5cbb644387c18e1
RaghulKarunakarn <raghul.k@doodleblue.com>
2025-07-06 20:16:42 +0530

    added interceptors to fix stale token issue

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,6 +3,16 @@ import axios from 'axios';
 export const axiosInstance = axios.create({
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
     }
 });
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
